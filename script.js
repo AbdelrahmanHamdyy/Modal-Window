48ee17e0c8c3af48ab69c0d100aff76b769760e3
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-07-23 17:25:32 +0200

    Close Modals and Code refactoring

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden'); // Can remove multiple classes separated by commas
+  overlay.classList.remove('hidden');
+  // modal.style.display = 'block';
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
-    modal.classList.remove('hidden'); // Can remove multiple classes separated by commas
-    overlay.classList.remove('hidden');
-    // modal.style.display = 'block';
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
