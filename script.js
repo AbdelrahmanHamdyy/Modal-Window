2f0b2f041a88f0c3670d54e1d1c37e3edbedf254
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-07-23 17:20:25 +0200

    Show Modal Window

--- script.js
+++ script.js
@@ -6,4 +6,9 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Button Clicked');
+    modal.classList.remove('hidden'); // Can remove multiple classes separated by commas
+    overlay.classList.remove('hidden');
+    // modal.style.display = 'block';
+  });
